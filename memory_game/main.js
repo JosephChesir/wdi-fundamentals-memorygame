e46049c995fa688cc68da5f90f319fbf7a6b45ff
[Joseph Chesir] <[joseph.chesir@mail.yu.edu]>
2020-01-08 17:43:14 -0500

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+
+
+let cardOne = "king";
+let cardTwo= "queen";
+let cardThree= "king";
+let cardFour= "queen";
+
+
+console.log("user flipped "+ cardOne);
+console.log("user flipped " + cardTwo)

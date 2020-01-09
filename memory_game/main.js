d25b2e2cc9f2a296fefcfc2975acf57694e137d8
[Joseph Chesir] <[joseph.chesir@mail.yu.edu]>
2020-01-09 08:30:00 -0500

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,11 +1,27 @@
 console.log("Up and running!");
 
 
-let cardOne = "king";
-let cardTwo= "queen";
-let cardThree= "king";
-let cardFour= "queen";
 
 
-console.log("user flipped "+ cardOne);
-console.log("user flipped " + cardTwo)
+
+cards= ["queen", "queen","king","king"]
+
+cardsInPlay= []
+
+cardOne= cards[0]
+
+cardsInPlay.push(cardOne)
+
+console.log("user flipped queen")
+
+cardTwo= cards[2]
+cardsInPlay.push(cardTwo)
+
+console.log("user fipped king")
+
+if (cardsInPlay[0]=== cardsInPlay[1]){
+	alert("You found a match!")
+
+} else {
+	alert("Sorry, try again")
+}

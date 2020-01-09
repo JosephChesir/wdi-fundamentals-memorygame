dc1ac6e49fa73e1da4b6ed985ebd0db4a56317c3
[Joseph Chesir] <[joseph.chesir@mail.yu.edu]>
2020-01-09 09:47:21 -0500

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -5,23 +5,26 @@ console.log("Up and running!");
 
 
 cards= ["queen", "queen","king","king"]
+cardsInPlay = []
 
-cardsInPlay= []
+function checkForMatch(){
 
-cardOne= cards[0]
-
-cardsInPlay.push(cardOne)
-
-console.log("user flipped queen")
-
-cardTwo= cards[2]
-cardsInPlay.push(cardTwo)
-
-console.log("user fipped king")
-
-if (cardsInPlay[0]=== cardsInPlay[1]){
-	alert("You found a match!")
-
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
 } else {
-	alert("Sorry, try again")
+  console.log("Sorry, try again.");
 }
+
+}
+
+
+const flipCard = (cardId) => {
+  console.log("user flipped" + cards[cardId])
+  if (cardsInPlay[0] === cardsInPlay[1]){
+  	alert("You found a match!")
+  } else {
+  	alert("Sorry, try again")
+  }
+}
+flipCard(0)
+flipCard(2)
